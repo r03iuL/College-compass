22eb067f4a0f12a5a8cb1b7f5781a2cd8c0830e8
r03iuL <robiulislam1806@gmail.com>
2023-07-24 23:33:46 +0600

    Added search

--- src/Layout/Home/Banner/Banner.jsx
+++ src/Layout/Home/Banner/Banner.jsx
@@ -1,24 +1,33 @@
- 
 const Banner = () => {
   return (
     <div>
       <div
         className="hero min-h-screen"
         style={{
-          backgroundImage:
-            "url(/Group.jpg)",
+          backgroundImage: "url(/Group.jpg)",
         }}
       >
-        <div className="hero-overlay bg-opacity-50"></div>
+        <div className="hero-overlay bg-opacity-70"></div>
         <div className="hero-content text-center text-neutral-content">
-          <div className="max-w-md">
-            <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-            <p className="mb-5">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
+          <div className="text-justify">
+            <h1 className="mb-5 text-5xl font-bold text-amber-400">
+              Welcome to College Campus.{" "}
+            </h1>
+            <p className="mb-5 text-xl text-amber-300 font-semibold ">
+              Your journey to finding the perfect college starts here. At
+              College Compass, we are dedicated to providing you with all the
+              essential details and guidance you need to make informed decisions
+              about your future. Discover a diverse range of colleges, explore
+              their programs, facilities, and campus life. Our comprehensive
+              database is designed to help you find the college that aligns with
+              your interests and aspirations.
             </p>
-            <button className="btn btn-primary">Get Started</button>
+            <input
+              type="text"
+              placeholder="Type here"
+              className="input input-bordered text-black font-semibold w-full max-w-xs my-3 px5"
+            />
+            <button className=" mx-2 btn bg-red-400 border-2">Search College</button>
           </div>
         </div>
       </div>
