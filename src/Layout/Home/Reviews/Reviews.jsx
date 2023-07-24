673d4b766bcce6392174c975065e6374fed34b3a
r03iuL <robiulislam1806@gmail.com>
2023-07-24 23:59:34 +0600

    Opdate

--- src/Layout/Home/Reviews/Reviews.jsx
+++ src/Layout/Home/Reviews/Reviews.jsx
@@ -1,17 +1,19 @@
 import Grids from "./Grids";
 
-
 const Reviews = () => {
   return (
     <div className="mx-20 ">
-        <h1 className="text-4xl font-bold my-5">Reviews.</h1>
+      <h1 className="text-4xl font-bold my-5">Reviews.</h1>
       <div className="flex flex-col items-center my-3">
         <select className="select select-bordered w-full max-w-3xl mx-auto">
           <option disabled selected>
             Select College
           </option>
-          <option>Han Solo</option>
-          <option>Greedo</option>
+          <option>Oakdale College</option>
+          <option>Ivywood College</option>
+          <option>Mapleview College </option>
+          <option>Brookside College </option>
+          <option>Cedarhill College </option>
         </select>
       </div>
       <div>
